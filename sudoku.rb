fb0df8f7f73767d294f92064ce9210609adedf7b
App Academy Student <student@appacademy.io>
2016-11-17 15:45:53 -0800

    Added parse_pos method and parse_val method to sudoku.  Added rows method to board.  And deleted alias call in board.

--- sudoku.rb
+++ sudoku.rb
@@ -1,7 +1,6 @@
 require_relative "board"
 require 'colorize'
 
-puts "Only contractors write code this bad.".yellow
 
 class SudokuGame
   def self.from_file(filename)
@@ -10,7 +9,7 @@ class SudokuGame
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
   def method_missing(method_name, *args)
@@ -68,6 +67,15 @@ class SudokuGame
     board.solved?
   end
 
+  def parse_val(string)
+    Integer(string)
+  end
+
+  def parse_pos(string)
+    string.split(",").map {|num| Integer(num)}
+  end
+
+
   def valid_pos?(pos)
     pos.is_a?(Array) &&
       pos.length == 2 &&
@@ -85,3 +93,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
